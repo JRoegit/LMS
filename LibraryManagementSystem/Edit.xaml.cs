4f8dcb471b2c9ffaffb10b167bb7561cbbf525c4
Jacob <jacobroe@rogers.com>
2024-02-03 21:38:50 -0500

    Built book search section, and condensed some tabs into that section

--- LibraryManagementSystem/Edit.xaml.cs
+++ LibraryManagementSystem/Edit.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,45 @@ namespace LibraryManagementSystem
 		public Edit()
 		{
 			InitializeComponent();
+
+			//resultGrid;  MAKE RESULT GRID FOR RETURN FROM DB WHEN SEARCHING
+			//
+			//List<BookInfo> books = new List<BookInfo>();
+			//
+			//
+			//
+		}
+		private string title;
+		private string author;
+		private string ISBN;
+		private string language;
+		private string description;
+		private string pubDate;
+		private List<string> genres;
+		private void Add_Book_Button_Click(object sender, RoutedEventArgs e)
+		{
+			title = bkTitle.Text;
+			author = bkAuthor.Text;
+			ISBN = bkISBN.Text;
+			language = bkLanguage.Text;
+			description = bkDesc.Text;
+			genres = bkGenre.Text.Split(',',' ').ToList<string>();
+			pubDate = bkPub.Text;
+			string giantmess = title + author + description + language + ISBN + pubDate + genres;
+			MessageBox.Show(giantmess);
+			//Check if ISBN already exists in db, then attempt to add book to db, uppon success, pop up window saying success
+		
+		}
+
+		public class BookInfo // FOR RETURN FROM DB????
+		{
+			public string Title { get; set; }
+			public string Author { get; set; }	
+			public string Description { get; set; }
+			public string ISBN {  get; set; }
+			public string[] Genres { get; set; }
+			public DateTime PUBDATE { get; set;}
+
 		}
 	}
 }
